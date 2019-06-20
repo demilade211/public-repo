4494412de7b3b3595f2cbb5059ee9a38e4b23865
demilade211 <50405225+demilade211@users.noreply.github.com>
2019-06-20 12:27:22 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-alert('HI');
 let btn = document.querySelector(".btn");
 let body = document.querySelector("body");
 btn.addEventListener("click", showTipCalculator);
 function showTipCalculator(){
-    body.style.opacity = "0.5"
+    body.style.opacity = "0.5";
     
 }
