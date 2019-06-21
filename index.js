6f15268f835879ba73488227c17e207519a7c977
demilade211 <50405225+demilade211@users.noreply.github.com>
2019-06-21 14:14:29 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-let btn = document.querySelector(".btn");
-let body = document.querySelector("body");
+let btn = document.querySelector("#btn");
+let body = document.querySelector("section");
+let con = document.querySelector(".form-container");
+let btns = document.querySelector("#btns");
+let name = document.querySelector(".name").value;
+let server = document.querySelector(".server").value;
+let amount = document.querySelector(".amount").value;
+let perc = document.querySelector(".perc").value;
+const percentage = 0.01;
+var tip;
 btn.addEventListener("click", showTipCalculator);
+btns.addEventListener("click", calcTip);
 function showTipCalculator(){
-    body.style.opacity = "0.5";
+    con.style.display = "flex";
+
     
+}
+function calcTip(){
+    tip = perc * percentage * amount;
+alert(`Hello ${name} you left a tip of ${tip} to ${server}`)
+
 }
